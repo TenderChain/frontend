cc887923fb9877d9b918ca4a500218c1c23d9013
Kaaml <kamiladam11@gmail.com>
2018-05-19 19:04:58 +0200

    add tender view

---
+++ src/components/MultiFileUpload.vue
@@ -0,0 +1,19 @@
+<script>
+  export default {
+    name: 'MultiFile',
+    data: () => ({
+      initial: 'vue-material-is-awesome.jpg',
+      single: null,
+      placeholder: null,
+      disabled: null,
+      multiple: null
+    })
+  }
+</script>
+
+<template>
+    <md-field>
+      <label>Attach documents</label>
+      <md-file v-model="multiple" multiple />
+    </md-field>
+</template>
